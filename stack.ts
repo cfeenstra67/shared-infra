d660050d4d021b99cfd3f27f68322950444b88a1
cfeenstra67 <cameron.l.feenstra@gmail.com>
2025-06-21 12:21:46 -0700

    moved camfeenstra stuff from website

--- stack.ts
+++ stack.ts
@@ -1,16 +1,41 @@
+import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 import * as saws from "@stackattack/aws";
 
 export default () => {
   const ctx = saws.context();
+  const config = new pulumi.Config();
+
+  const gmailVerificationCode = config.require("gmail-verification-code");
 
   const vpc = saws.vpc(ctx);
   const vpn = saws.vpn(ctx, vpc);
 
+  const camFeenstra = "camfeenstra.com";
+
   const camFeenstraCertificate = saws.certificate(ctx.prefix("camfeenstra"), {
-    domain: "camfeenstra.com",
-    noValidate: true,
+    domain: camFeenstra,
     wildcard: true,
   });
+
+  saws.gmailDomain(ctx, {
+    domain: camFeenstra,
+    verificationCode: gmailVerificationCode,
+  });
+
+  const east = new aws.Provider(ctx.id("east-provider"), {
+    region: "us-east-1",
+  });
+
+  const camFeenstraEastCertificate = saws.certificate(
+    ctx.prefix("camfeenstra-east"),
+    {
+      domain: camFeenstra,
+      noValidate: true,
+      wildcard: true,
+      provider: east,
+    },
+  );
 
   const loadBalancer = saws.loadBalancer(ctx, {
     network: vpc.network("public"),
@@ -39,5 +64,6 @@ export default () => {
     database: saws.databaseToIds(database),
     redisUrl: redis.url,
     camFeenstraCertificate,
+    camFeenstraEastCertificate,
   };
 };
